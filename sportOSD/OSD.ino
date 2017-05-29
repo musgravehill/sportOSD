8860643d709482eeebd0cf030aa42729fb3c0092
Bob_work <musgravehill@gmail.com>
2017-05-29 15:25:19 +0400

    osd

--- sportOSD/OSD.ino
+++ sportOSD/OSD.ino
@@ -55,11 +55,15 @@ void OSD_vsync_interrupt() {
 
 void OSD_render() {
   if (OSD_isRenderAllow) {
-    //stuff is here
     OSD_isRenderAllow = false; //waiting for next Vsync interrupt event
+    if (SYS_GPS_isHomeFixed) {
+      
+    } else {
+      osd.print("GPS HOME POS PROCESSING", 1, 3);
+    }
   }
 
-  ////////////TEST!!!!!!!!! 
+  ////////////TEST!!!!!!!!!
   osd.printMax7456Char(0x01, 0, 1);
   osd.print("Hello world :)", 1, 3);
   osd.print("Current Arduino time :", 1, 4);
@@ -67,5 +71,5 @@ void OSD_render() {
   osd.printMax7456Char(0xD1, 9, 6, true);
   osd.print("00'00\"", 10, 6);
   byte tab[] = {0xC8, 0xC9};
-  osd.printMax7456Chars(tab, 2, 12, 7);  
+  osd.printMax7456Chars(tab, 2, 12, 7);
 }
