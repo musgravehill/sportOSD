7f578eea85c640fc0eaf9c21c327994e973bc4af
Bob_work <musgravehill@gmail.com>
2017-06-05 15:45:51 +0400

    adc

--- sportSensors/sportArduSensor/ADC.ino
+++ sportSensors/sportArduSensor/ADC.ino
@@ -5,7 +5,7 @@ void ADC_process() {
     0<=alpha<=1
     alpha = 2 /(N+1), where N = average window size
   */
-  SYS_acc_main_v = 0.2f * SYS_acc_main_v  + 0.8f * 0.01362683f * analogRead(SYS_ADC_PIN_acc_main); //13V ---> 4.66V by divider ----> 954 points
-  SYS_acc_video_v = 0.2f * SYS_acc_video_v + 0.8f * 0.0048828125f * analogRead(SYS_ADC_PIN_acc_video); //5v ---> 1024 points
+  SYS_acc_main_v = 0.1f * SYS_acc_main_v  + 0.9f * 0.01362683f * analogRead(SYS_ADC_PIN_acc_main); //13V ---> 4.66V by divider ----> 954 points
+  SYS_acc_video_v = 0.1f * SYS_acc_video_v + 0.9f * 0.0048828125f * analogRead(SYS_ADC_PIN_acc_video); //5v ---> 1024 points
 }
 
