fc9735ba80465a77080054aba4a33324b2f17a28
Bob_work <musgravehill@gmail.com>
2019-09-26 17:13:20 +0300

    v2

---
+++ RTFM2019/FrSky S-Port telemetry library - easy to use and configurable - RC Groups_files/adjsc.php
@@ -0,0 +1,11 @@
+function phpads_deliverActiveX(content) {document.write(content);}
+var OA_output = new Array();
+OA_output['width:700-950,_height:70-110,_rcgf-135-img|rcgf-adsense'] = '';
+OA_output['width:700-950,_height:70-110,_rcgf-135-img|rcgf-adsense'] += '<'+'a href=\'https://decals.rcgroups.net//adclick.php?bannerid=26285&amp;zoneid=0&amp;source=Radios&amp;dest=http%3A%2F%2Fwww.espritmodel.com%2Fjeti-model-cz.aspx\' target=\'_blank\' onMouseOver="self.status=\'Jeti Duplex Receivers with System Integration, Telemetry &amp; Stabilization\'; return true;" onMouseOut="self.status=\'\';return true;"><'+'img class=\'_decalimg\' src=\'https://decals.rcgroups.net/storage/jeti_rx.jpg\' width=\'950\' height=\'110\' alt=\'Jeti Duplex Receivers with System Integration, Telemetry &amp; Stabilization\' title=\'Jeti Duplex Receivers with System Integration, Telemetry &amp; Stabilization\' border=\'0\'><'+'/a>';
+OA_output['400x25,_campaignid:2850'] = '';
+OA_output['zone:143'] = '';
+OA_output['zone:143'] += '<'+'a href=\'https://decals.rcgroups.net//adclick.php?bannerid=26703&amp;zoneid=143&amp;source=Radios&amp;dest=https%3A%2F%2Fwww.gravesrc.com%2F\' target=\'_blank\'><'+'img class=\'_decalimg\' src=\'https://decals.rcgroups.net/storage/rcg1_2.png\' width=\'450\' height=\'110\' alt=\'\' title=\'\' border=\'0\'><'+'/a>';
+OA_output['zone:174'] = '';
+OA_output['zone:174'] += '<'+'a href=\'https://decals.rcgroups.net//adclick.php?bannerid=24837&amp;zoneid=174&amp;source=Radios&amp;dest=http%3A%2F%2Fwww.clm-pro.com\' target=\'_blank\'><'+'img class=\'_decalimg\' src=\'https://decals.rcgroups.net/storage/clm-pro-new-pod-130-banner.png\' width=\'450\' height=\'110\' alt=\'\' title=\'\' border=\'0\'><'+'/a>';
+OA_output['zone:98'] = '';
+OA_output['clientid=1004'] = '';
